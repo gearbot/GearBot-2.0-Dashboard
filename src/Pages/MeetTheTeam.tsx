b97ad770b99fc436ddb73425f2a1d22cf273ef88
JohnyTheCarrot <tuurmartens4@gmail.com>
2020-06-22 12:12:23 +0200

    remove comment that was accidentally copied from the development page

--- src/Pages/MeetTheTeam.tsx
+++ src/Pages/MeetTheTeam.tsx
@@ -1,9 +1,3 @@
-/**
- * This page is for MeetTheTeam purposes, you can freely test your components here.
- * This will be shipped with the final product, and will even resolve when requested at /__MeetTheTeam.
- * There will be no navigation pointing to this page.
- */
-
 import React from "react";
 import { GearTeamMember } from "../Other/Types";
 import TeamGearBot from "../Data/teamgearbot.json";
